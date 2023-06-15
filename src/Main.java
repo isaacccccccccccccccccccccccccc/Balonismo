95c3b8321f8539a75dac4a8320b52d2da022c277
isaacccccccccccccccccccccccccc <isaacalmeidabarros05@gmail.com>
2023-06-15 11:51:02 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    public static void main(String[]args){
+        //Criar objetos ao passeio
+        Pessoa p1 = new Pessoa();
+        Pessoa p2; // Declaração do objeto
+        p2 = new Pessoa(); // construção =
+        //...
+        p1.nome ="Elliot Page"
+    }
+}
